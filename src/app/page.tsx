42bdc7868cd4e7be2c539426730dfa6993c94982
feneel <feneelp@gmail.com>
2024-12-15 16:07:14 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import Clients from "@/sections/Clients";
-import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
-import LogoTicker from "@/sections/LogoTicker";
-import Noise from "@/sections/Noise";
 import Offline from "@/sections/Offline";
-import Products from "@/sections/Products";
 import UseCase from "@/sections/UseCase";
-import WhyOffline from "@/sections/WhyOffline";
 
 export default function Home() {
   return (
