92ad66e0418771d1ba794b0c46e242f951fa21bc
feneel <feneelp@gmail.com>
2024-12-15 14:55:01 +0530

    first commit

--- src/components/ProductsDetails.tsx
+++ src/components/ProductsDetails.tsx
@@ -17,9 +17,9 @@ const ProductsDetails: FC<ProductsDetailsProps> = ({ product }) => {
       <div className="py-20 flex flex-col lg:flex-row gap-10 border-b border-gray-200/20">
         <div className="lg:w-[550px] lg:border-r border-gray-200/20 flex items-start lg:shrink-0 ">
           <div className="flex flex-col gap-6 lg:sticky lg:top-4 transition-all duration-200 ease-in-out">
-            <Heading>
+            {/* <Heading>
               <h1>{product.name}</h1>
-            </Heading>
+            </Heading> */}
             <div className="flex flex-col gap-8">
               {product.featurepoints.map((point, index) => {
                 return (
@@ -29,9 +29,9 @@ const ProductsDetails: FC<ProductsDetailsProps> = ({ product }) => {
                 );
               })}
             </div>
-            <ShinyButton href="/products" className="w-40 h-10">
+            {/* <ShinyButton href="/products" className="w-40 h-10">
               Get In Touch
-            </ShinyButton>
+            </ShinyButton> */}
           </div>
         </div>
         <div className="flex flex-col lg:pl-20 w-full gap-20">
@@ -48,8 +48,12 @@ const ProductsDetails: FC<ProductsDetailsProps> = ({ product }) => {
               </Paragraph>
             </div>
           </div>
+
+          <ProductCarousel
+            productComponents={product.components}
+          ></ProductCarousel>
           <div className="">
-            <div className="relative w-full h-[600px] ">
+            <div className="relative w-full h-[400px] ">
               <div className="absolute inset-0 p-10 z-10 text-black font-oswald text-5xl flex flex-col gap-4">
                 <h1>{product.imageTitle}</h1>
                 <p className="text-xl lg:w-1/2">{product.imageDesc}</p>
@@ -70,9 +74,6 @@ const ProductsDetails: FC<ProductsDetailsProps> = ({ product }) => {
               ></Image>
             </div>
           </div>
-          <ProductCarousel
-            productComponents={product.components}
-          ></ProductCarousel>
           <div className="w-full mx-auto border border-gray-200/20"></div>
         </div>
       </div>
--- src/lib/products.ts
+++ src/lib/products.ts
@@ -5,7 +5,7 @@ export const PRODUCTS = [
     paragraph1:
       "The Integrated Voice Module is a versatile solution designed to bring smart voice control to your devices. Engineered for seamless integration, it enables voice-activated functionalities with robust processing capabilities, making everyday interactions effortless and intuitive.",
     paragraph2:
-      "The Integrated Voice Module offers offline voice processing, allowing reliable performance without internet dependency. It provides precise control for devices like fans and supports communication via UART or other protocols for seamless on device communication. Equipped with single-language support, it also offers the flexibility to upgrade to multiple languages.",
+      "The Integrated Voice Module offers offline voice processing, allowing reliable performance without internet dependency. It provides precise control for devices like fans and supports communication via UART or other protocols for seamless on device communication. Equipped with single-language support, it also offers the flexibility to upgrade to multiple languages. Advanced features like noise suppression and active gain control ensure clear voice recognition even in noisy environments. Use of digital microphone delivers high-quality audio input for optimal performance.",
     image: "/circuit.png",
     imageTitle: "Integrated Voice Module",
     imageDesc:
@@ -60,35 +60,35 @@ const components = [
   {
     name: "Aster",
     features: [
-      "Compact voice module for simplifying and smartening home appliances",
-      "Offline voice processing for consistent and reliable performance without internet",
       "Built with a single digital microphone",
+      "Single-language support for precise voice control",
+      "Offline voice processing for consistent and reliable performance without internet",
+      "Compact voice module for simplifying and smartening home appliances",
       "Supports seamless communication via UART or other protocols",
       "Ideal for integration into existing appliances like fans",
-      "Single-language support for precise voice control",
       "Equipped with noise suppression and active gain control for clear command recognition",
     ],
   },
   {
     name: "Bellis",
     features: [
-      "Advanced voice module for enhanced voice control in smart home devices",
       "Offline voice processing with dual digital microphones",
+      "Dual-language recognition for versatility among diverse users",
+      "Advanced voice module for enhanced voice control in smart home devices",
       "Improved far-field recognition and superior command accuracy",
       "Supports seamless connectivity via UART or similar protocols",
-      "Dual-language recognition for versatility among diverse users",
-      "Equipped with noise suppression and active gain control for clear voice input",
       "Ideal for upgrading traditional appliances like fans into modern smart systems",
+      "Equipped with noise suppression and active gain control for clear voice input",
     ],
   },
   {
     name: "Dahlia",
     features: [
-      "Premium voice module with next-generation voice capabilities",
       "Offline voice processing with four digital microphones",
-      "Excels in far-field voice recognition for larger spaces",
       "Multi-language recognition for diverse users and global applications",
-      "Supports seamless integration via UART or other communication protocols",
+      "Premium voice module with next-generation voice capabilities",
+      "Excels in far-field voice recognition for larger spaces",
+      "Supports communication via UART or other protocols, ensuring seamless on-device connectivity.",
       "Transforms appliances like fans into intelligent systems",
       "Advanced noise suppression and active gain control for crystal-clear voice input",
     ],
